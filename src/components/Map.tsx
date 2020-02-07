53fff8a7b0b30c2eeebe8fb5f5167a46ade0ba38
umutcanbolat <10065235+umutcanbolat@users.noreply.github.com>
2020-02-08 00:28:38 +0300

    feat: 🎸 highlight countries on hover

--- src/components/Map.tsx
+++ src/components/Map.tsx
@@ -1,19 +1,28 @@
-import React, { useState, useCallback } from 'react';
+import React, { useState, useCallback, useMemo } from 'react';
 import MapGL, { Source, Layer, ViewportProps, PointerEvent } from 'react-map-gl';
 import { FeatureCollection, Geometry } from 'geojson';
 import find from 'lodash/find';
-import countriesRaw from '../assets/maps/countries.geo.json';
+import countriesRaw from '../assets/maps/countries.geo.min.json';
 
 const MAPBOX_TOKEN = process.env.token;
 const countriesJSON = countriesRaw as FeatureCollection<Geometry>;
-const countriesLayerId = 'countries.geo.json';
+const countriesSourceId = 'countries.geo.json';
 
-export const countriesLayer = {
-  id: 'point',
+const countriesLayer = {
+  id: 'constant-layer',
   type: 'fill',
   paint: {
     'fill-outline-color': 'rgba(0,0,0,0.1)',
-    'fill-color': 'rgba(0,0,0,0.1)',
+    'fill-color': 'rgba(0,0,0,0)',
+  },
+};
+
+const hoverLayer = {
+  id: 'hover-layer',
+  type: 'fill',
+  paint: {
+    'fill-outline-color': 'rgba(0,0,0,0.1)',
+    'fill-color': 'rgba(0,0,0,0.2)',
   },
 };
 
@@ -26,6 +35,8 @@ const Map: React.FC<{}> = () => {
     pitch: 0,
   });
 
+  const [hoverCountry, setHoverCountry] = useState({ countryName: '', countryCode: '' });
+
   const onViewportChange = useCallback((viewState: ViewportProps): void => {
     setViewport(viewState);
   }, []);
@@ -33,39 +44,50 @@ const Map: React.FC<{}> = () => {
   const onHover = useCallback((event: PointerEvent): void => {
     const mouseLocation: [number, number] = event.lngLat;
     let countryName = '';
+    let countryCode = '';
 
     let country = null;
     if (event.features) {
-      country = find(event.features, ['source', countriesLayerId]);
+      country = find(event.features, ['source', countriesSourceId]);
     }
 
-    if (country) {
-      countryName = country.properties ? country.properties.name : '';
+    if (country && country.properties) {
+      countryName = country.properties.name;
+      countryCode = country.properties.code;
+      setHoverCountry({ countryName, countryCode });
+    } else {
+      setHoverCountry({ countryName: '', countryCode: '' });
     }
 
-    console.log(countryName, mouseLocation);
+    // console.log(countryName, countryCode, mouseLocation);
   }, []);
 
-  return (
-    <>
-      <MapGL
-        {...viewport}
-        width="inherit"
-        height="inherit"
-        mapStyle="mapbox://styles/mapbox/streets-v11"
-        onViewportChange={onViewportChange}
-        mapboxApiAccessToken={MAPBOX_TOKEN}
-        onHover={onHover}
-      >
-        <Source id={countriesLayerId} type="geojson" data={countriesJSON}>
-          {/* beforeId adds countries layer before the waterway-label. */}
-          {/* This prevents country or city names from being hidden under our geojson layer */}
-          <Layer beforeId="waterway-label" {...countriesLayer} />
-          {/* <Layer {...highlightLayer} filter={filter} /> */}
-        </Source>
-      </MapGL>
-    </>
-  );
+  return useMemo(() => {
+    return (
+      <>
+        <MapGL
+          {...viewport}
+          width="inherit"
+          height="inherit"
+          mapStyle="mapbox://styles/mapbox/light-v9"
+          onViewportChange={onViewportChange}
+          mapboxApiAccessToken={MAPBOX_TOKEN}
+          onHover={onHover}
+        >
+          <Source id={countriesSourceId} type="geojson" data={countriesJSON}>
+            {/* beforeId adds countries layer before the waterway-label. */}
+            {/* This prevents country or city names from being hidden under our geojson layer */}
+            <Layer beforeId="waterway-label" {...countriesLayer} />
+            <Layer
+              beforeId="waterway-label"
+              {...hoverLayer}
+              filter={['==', 'name', hoverCountry.countryName]}
+            />
+          </Source>
+        </MapGL>
+      </>
+    );
+  }, [viewport, hoverCountry]);
 };
 
 export default Map;
